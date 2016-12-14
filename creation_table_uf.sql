e0a74b619670ded611caf58603a0a1d94ea67c20
Gosset <gosset@enst.fr>
2016-12-14 10:05:08 +0100

    ajout colonne table uf : liste des idpar (array)

--- creation_table_uf.sql
+++ creation_table_uf.sql
@@ -11,7 +11,8 @@ create table fd94.pnb_uf_2013 as
 		concat(max(idpar), count(idpar), cast(avg(st_area(geompar)) as int)) iduf,
 		max(idpar) idpar,
 		2013 annee,
-		count(idpar) npar,  -- nombre de parcelles composant l'UF
+		array_agg(idpar) idpar_l,	-- array des idpar constituant l'uf
+		count(idpar) npar,  		-- nombre de parcelles composant l'UF
 		sum(nlocal) nlocal,
 		sum(nlocmaison) nlocmaison,
 		sum(nlocappt) nlocappt,
@@ -39,36 +40,81 @@ create table fd94.pnb_uf_2013 as
 	group by geom
 ;
 
-delete from fd94.pnb_uf_2013
-where (idpar, geom) in (
-	select uf.idpar, uf.geom
-	from (
-		select idpar, max(nlocal) nloc, max(npar) npar, max(st_area(geom)) surf
-		from fd94.pnb_uf_2013
-		group by idpar
-		having count(*)>1
-		) foo,
-		fd94.pnb_uf_2013 uf
-	where uf.idpar=foo.idpar
-	except
-	select uf.idpar, uf.geom
-	from (
-		select idpar, max(nlocal) nloc, max(npar) npar, max(st_area(geom)) surf
-		from fd94.pnb_uf_2013
-		group by idpar
-		having count(*)>1
-		) foo,
-		fd94.pnb_uf_2013 uf
-	where uf.idpar=foo.idpar and foo.nloc=uf.nlocal and foo.npar=uf.npar and surf=st_area(uf.geom)
-	);
-
-
-ALTER TABLE fd94.pnb_uf_2013 ADD PRIMARY KEY (idpar);
-
-CREATE INDEX pnb_uf_2013_idpar_idx ON fd94.pnb_uf_2013 (idpar);
-
+-- création d'un index spatial
 CREATE INDEX pnb_uf_2013_geompar_idx
   ON fd94.pnb_uf_2013
   USING gist
   (geom);
 
+-- suppression des inclusions
+delete from fd94.pnb_uf_2013
+where geom in (
+	select p2.geom
+	from fd94.pnb_uf_2013 p1, fd94.pnb_uf_2013 p2
+	where st_contains(p1.geom,p2.geom) and not st_equals(p1.geom, p2.geom)
+	);
+delete from fd94.pnb_uf_2013
+where idpar in (
+	select idpar
+	from fd94.pnb_uf_2013
+	group by idpar
+	having count(idpar)>1
+	);
+
+-- création d'une clé primaire et d'un index sur cette clé
+ALTER TABLE fd94.pnb_uf_2013 ADD PRIMARY KEY (idpar);
+
+CREATE INDEX pnb_uf_2013_idpar_idx ON fd94.pnb_uf_2013 (idpar);
+
+
+-- creation de colonnes caractérisant l'usage en 4, 8 et 16 classes
+alter table fd94.pnb_uf_2013
+add column idusage4 int;
+
+alter table fd94.pnb_uf_2013
+add column idusage8 int;
+
+alter table fd94.pnb_uf_2013
+add column idusage16 int;
+
+-- maj des colonnes de caractérisation des usages
+WITH classe4 AS (
+	SELECT 	iduf,
+		CASE nlocmaison+nlocappt WHEN 0 THEN 0 ELSE 1 END AS c1, 
+		CASE nloccom WHEN 0 THEN 0 ELSE 1 END AS c2
+	FROM fd94.pnb_uf_2013
+	),
+	classe8 AS (
+	SELECT 	iduf,
+		CASE nlocmaison WHEN 0 THEN 0 ELSE 1 END AS c1, 
+		CASE nlocappt WHEN 0 THEN 0 ELSE 1 END   AS c2, 
+		CASE nloccom WHEN 0 THEN 0 ELSE 1 END    AS c4
+	FROM fd94.pnb_uf_2013
+	),
+	classe16 AS (
+	SELECT 	iduf,
+		CASE nlocdep WHEN 0 THEN 0 ELSE 1 END AS c1, 
+		CASE nlocmaison WHEN 0 THEN 0 ELSE 1 END AS c2, 
+		CASE nlocappt WHEN 0 THEN 0 ELSE 1 END   AS c4, 
+		CASE nloccom WHEN 0 THEN 0 ELSE 1 END    AS c8
+	FROM fd94.pnb_uf_2013
+	)
+UPDATE fd94.pnb_uf_2013 u
+SET idusage4=foo.idusage4, idusage8=foo.idusage8, idusage16=foo.idusage16
+FROM (
+	SELECT 
+		u1.iduf as iduf,
+		us1.idusage as idusage4,
+		us2.idusage as idusage8,
+		us3.idusage as idusage16
+	FROM 
+		classe4 c1, classe8 c2, classe16 c3,
+		fd94.usage_4classes us1, fd94.usage_8classes us2, fd94.usage_16classes us3,
+		fd94.pnb_uf_2013 u1
+	WHERE 
+		u1.iduf=c1.iduf AND u1.iduf=c2.iduf AND u1.iduf=c3.iduf
+		AND c1.c1 + 2*c1.c2 = us1.idusage 
+		AND c2.c1 + 2*c2.c2 + 4*c2.c4= us2.idusage
+		AND c3.c1 + 2*c3.c2 + 4*c3.c4 + 8*c3.c8 = us3.idusage
+	) AS foo
+WHERE foo.iduf=u.iduf;
